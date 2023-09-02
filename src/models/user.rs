d1f0ec6d3819d0313b84e4626cecd4115f61f015
Benjamin Hajdukiewicz <70604257+katanallama@users.noreply.github.com>
2023-09-02 06:16:52 -0600

    move user proc

--- src/models/user.rs
+++ src/models/user.rs
@@ -1,5 +1,9 @@
 // models/user.rs
 //
+use std::collections::HashMap;
+use std::fs::OpenOptions;
+use std::io::prelude::*;
+
 use crate::models::response::CustomError;
 use crate::process_result::ProcessResult;
 use chrono::Utc;
@@ -15,6 +19,59 @@ pub struct User {
     pub url: String,
     pub wstoken: String,
     pub lastfetched: i64,
+}
+
+#[derive(Deserialize, Debug)]
+pub struct Course {
+    pub id: i64,
+    pub courseid: Option<i64>,
+    pub shortname: String,
+    pub fullname: String,
+    pub lastfetched: i64,
+}
+
+pub fn write_course_conf(courses: Vec<Course>) -> Result<ProcessResult, CustomError> {
+    let mut file = OpenOptions::new()
+        .write(true)
+        .create(true)
+        .open("moodl-rs-id")?;
+
+    let mut course_map = HashMap::new();
+    for course in &courses {
+        if let Some(course_id) = course.courseid {
+            course_map.insert(course_id, &course.shortname);
+        }
+    }
+
+    file.write_all(b"Course Map: {\n")?;
+    for (course_id, course_name) in &course_map {
+        file.write_all(format!("    {}: {},\n", course_id, course_name).as_bytes())?;
+    }
+    file.write_all(b"}\n")?;
+
+    Ok(ProcessResult::Courses(courses))
+}
+
+pub fn process_courses(_conn: &Connection, content: &str) -> Result<ProcessResult, CustomError> {
+    let parsed: Value = serde_json::from_str(content)?;
+
+    let mut course_list: Vec<Course> = Vec::new();
+    let now = Utc::now().timestamp();
+
+    if let Some(courses) = parsed.as_array() {
+        for course in courses {
+            let course = Course {
+                id: 0,
+                courseid: course["id"].as_i64(),
+                shortname: course["shortname"].to_string(),
+                fullname: course["fullname"].to_string(),
+                lastfetched: now,
+            };
+            course_list.push(course);
+        }
+    }
+
+    Ok(ProcessResult::Courses(course_list))
 }
 
 pub fn process_user(_conn: &Connection, content: &str) -> Result<ProcessResult, CustomError> {
