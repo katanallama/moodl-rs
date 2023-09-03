4542b53e8ecd1b7e19944cbdf7da03e872726762
Benjamin Hajdukiewicz <70604257+katanallama@users.noreply.github.com>
2023-09-03 05:38:47 -0600

    refactor parser

--- src/ui/parser.rs
+++ src/ui/parser.rs
@@ -1,7 +1,6 @@
 // ui/parser.rs
 use crate::models::course_content::CourseModule;
 use crate::models::response::CustomError;
-// use crate::ui::tui::*;
 use html2md::parse_html;
 use regex;
 use regex::Regex;
@@ -14,10 +13,16 @@ pub struct ParsedModule {
     pub url: Option<String>,
 }
 
-pub fn fetch_and_parse_modules(
+pub struct ParsedPage {
+    pub name: String,
+    pub description: String,
+    pub url: Option<String>,
+}
+
+fn fetch_modules(
     conn: &Connection,
     course_id: i32,
-) -> Result<Vec<ParsedModule>, CustomError> {
+) -> Result<Vec<CourseModule>, CustomError> {
     let mut stmt = conn.prepare(
         "SELECT id, courseid, moduleid, modulename, content, lastfetched FROM Modules WHERE courseid = ?1",
     )?;
@@ -32,55 +37,91 @@ pub fn fetch_and_parse_modules(
         })
     })?;
 
+    let mut modules = Vec::new();
+    for row_result in rows {
+        match row_result {
+            Ok(module) => modules.push(module),
+            Err(e) => return Err(CustomError::from(e)), // Assuming you have a from implementation for your error type
+        }
+    }
+
+    Ok(modules)
+}
+
+
+pub fn fetch_and_parse_modules(
+    conn: &Connection,
+    course_id: i32,
+) -> Result<Vec<ParsedModule>, CustomError> {
+    let rows = fetch_modules(conn, course_id)?;
+    parse_modules(rows)
+}
+
+
+fn parse_modules(rows: Vec<CourseModule>) -> Result<Vec<ParsedModule>, CustomError> {
     let mut parsed_modules = Vec::new();
 
     for module in rows {
-        match module {
-            Ok(m) => {
-                let json_data: Value = serde_json::from_str(&m.content)?;
-
-                let description = if let Ok(unescaped_html) =
-                    serde_json::from_str::<String>(&json_data["description"].to_string())
-                {
-                    parse_html(&unescaped_html.replace(r#"<br>"#, r#""#))
-                } else {
-                    String::new()
-                };
-
-                let url = json_data["contents"]
-                    .as_array()
-                    .and_then(|contents| contents.get(0))
-                    .and_then(|content| content["fileurl"].as_str())
-                    .map(|s| s.to_string());
-
-                // Replace headers surrounded by #### with #
-                let header_re = Regex::new(r#"#### (.*?) ####"#).unwrap();
-                let cleaned_headers = header_re.replace_all(&description, "# $1");
-
-                let header_re = Regex::new(r#"### (.*?) ###"#).unwrap();
-                let cleaned_headers = header_re.replace_all(&cleaned_headers, "# $1");
-
-                // Replace excessive asterisks around bold text with just two asterisks
-                let bold_re = Regex::new(r#"\*\*\*\*\* (.*?) \*\*\*\*\*"#).unwrap();
-                let cleaned_bold = bold_re.replace_all(&cleaned_headers, "**$1**");
-
-                // Handle other specific cases
-                let other_re = Regex::new(r#"(\*\*\*\*\*)|(\*\*\*\*)"#).unwrap();
-                let description = other_re.replace_all(&cleaned_bold, "**");
-                parsed_modules.push(ParsedModule {
-                    name: m.modulename.replace(r#"""#, r#""#),
-                    description: description.to_string(),
-                    url,
-                });
-            }
-            Err(e) => {
-                eprintln!("Error fetching module: {}", e);
-            }
+        match parse_module(&module) {
+            Ok(parsed) => parsed_modules.push(parsed),
+            Err(e) => eprintln!("Error parsing module: ",),
         }
     }
 
     Ok(parsed_modules)
 }
+
+fn parse_module(module: &CourseModule) -> Result<ParsedModule, CustomError> {
+    let json_data: Value = serde_json::from_str(&module.content)?;
+
+    let description = parse_description(&json_data)?;
+    let url = extract_url(&json_data);
+
+    Ok(ParsedModule {
+        name: module.modulename.replace(r#"""#, r#""#),
+        description,
+        url,
+    })
+}
+
+fn parse_description(json_data: &Value) -> Result<String, CustomError> {
+    if let Ok(unescaped_html) = serde_json::from_str::<String>(&json_data["description"].to_string()) {
+        Ok(parse_html(&clean_description(&unescaped_html)))
+    } else {
+        Ok(String::new())
+    }
+}
+
+fn clean_description(description: &str) -> String {
+    let patterns = vec![
+        (r#"<br>"#, ""),
+        (r#"#### (.*?) ####"#, "# $1"),
+        (r#"### (.*?) ###"#, "# $1"),
+        (r#"\*\*\*\*\* (.*?) \*\*\*\*\*"#, "**$1**"),
+        (r#"(\*\*\*\*\*)|(\*\*\*\*)"#, "**"),
+    ];
+
+    let mut cleaned = description.to_string();
+
+    for (pattern, replace_with) in patterns {
+        let re = Regex::new(pattern).unwrap();
+        cleaned = re.replace_all(&cleaned, replace_with).to_string();
+    }
+
+    cleaned
+}
+
+fn extract_url(json_data: &Value) -> Option<String> {
+    json_data["contents"]
+        .as_array()
+        .and_then(|contents| contents.get(0))
+        .and_then(|content| content["fileurl"].as_str())
+        .map(|s| s.to_string())
+}
+
+
+
+
 
 fn print_modules(modules: &[ParsedModule]) {
     for module in modules {
